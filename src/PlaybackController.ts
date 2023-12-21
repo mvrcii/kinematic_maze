865e7361ec89ca636e4d259dc8a8aa10f9a1fd37
Foschyy <69677695+Foschyy@users.noreply.github.com>
2023-12-21 23:16:45 +0100

    new layout

--- src/PlaybackController.ts
+++ src/PlaybackController.ts
@@ -13,14 +13,13 @@ export function getQueryVariable(variable: any) {
 }
 
 export class PlaybackController {
-    motionVis1: MotionVisualization
-    motionVis2: MotionVisualization
+    motionVis: MotionVisualization
 
     playButton: HTMLButtonElement | null
     timerElement: TimerElement
     timelineElement: TimelineElement
 
-    constructor(motionVis1: MotionVisualization, motionVis2: MotionVisualization) {
+    constructor(motionVis: MotionVisualization, motionVis2: MotionVisualization) {
         this.playButton = document.querySelector("#playPause") as HTMLButtonElement;
         this.timerElement = new TimerElement(document.querySelector("#timer") as HTMLElement);
         this.timelineElement = new TimelineElement(document.querySelector("#timeline") as HTMLProgressElement);
@@ -29,34 +28,30 @@ export class PlaybackController {
         this.timelineElement.reactor.addEventListener("sweepRequest",
             (position: any) => this.onSweep(position));
 
-        this.motionVis1 = motionVis1;
-        this.motionVis1.reactor.addEventListener("step", () => this.onStep());
-        this.motionVis2 = motionVis2;
-        this.motionVis2.reactor.addEventListener("step", () => this.onStep());
+        this.motionVis = motionVis;
+        this.motionVis.reactor.addEventListener("step", () => this.onStep());
+        
     }
 
     onSweep(position: any) {
-        this.motionVis1.sweep(position);
-        this.motionVis2.sweep(position)
+        this.motionVis.sweep(position);
+        
     }
 
     onStep() {
-        if (this.motionVis1.isEverythingLoadedAndReady()) {
+        if (this.motionVis.isEverythingLoadedAndReady()) {
             this.timerElement.update(new Date(this.getCurrentSceneTimestamp()));
-            this.timelineElement.update(this.motionVis1.progress());
+            this.timelineElement.update(this.motionVis.progress());
         }
-        if (this.motionVis2.isEverythingLoadedAndReady()) {
-            this.timerElement.update(new Date(this.getCurrentSceneTimestamp()));
-            this.timelineElement.update(this.motionVis2.progress());
-        }
+        
     }
 
     getCurrentSceneTimestamp() {
-        return this.motionVis1.getCurrentTimestamp()
+        return this.motionVis.getCurrentTimestamp()
     }
 
     playPause() {
-        this.motionVis1.pauseContinue();
-        this.motionVis2.pauseContinue();
+        this.motionVis.pauseContinue();
+       
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -24,10 +24,48 @@ function initializeApp() {
     //     titleElement.textContent = `VR Dashboard - ${dataset}`;
     // }
 
+    // #### Fixed Datasets ###
+    const players = [];
 
-    const player1 = new MotionVisualization("samples/" + dataset + "/original.csv", "threeCanvas1");
-    const player2 = new MotionVisualization("samples/" + dataset + "/fixed.csv", "threeCanvas2");
-    const controller = new PlaybackController(player1, player2);
+    const player1 = new MotionVisualization("samples/liebers_1/fixed.csv", "threeCanvas1");
+    players.push(player1);
+    
+    const player2 = new MotionVisualization("samples/liebers_2/fixed.csv", "threeCanvas2");
+    players.push(player2);
+    
+    const player3 = new MotionVisualization("samples/liebers_3/fixed.csv", "threeCanvas3");
+    players.push(player3);
+    
+    const player4 = new MotionVisualization("samples/IVRUADST/fixed.csv", "threeCanvas4");
+    players.push(player4);
+    
+    const player5 = new MotionVisualization("samples/bsor/lukas.csv", "threeCanvas5");
+    players.push(player5);
+    
+    const player6 = new MotionVisualization("samples/r_miller/fixed.csv", "threeCanvas6");
+    players.push(player6);
+
+    const player1_orig = new MotionVisualization("samples/liebers_1/original.csv", "threeCanvas_orig1");
+    players.push(player1_orig);
+    
+    const player2_orig = new MotionVisualization("samples/liebers_2/original.csv", "threeCanvas_orig2");
+    players.push(player2_orig);
+    
+    const player3_orig = new MotionVisualization("samples/liebers_3/original.csv", "threeCanvas_orig3");
+    players.push(player3_orig);
+    
+    const player4_orig = new MotionVisualization("samples/IVRUADST/original.csv", "threeCanvas_orig4");
+    players.push(player4_orig);
+    
+    const player5_orig = new MotionVisualization("samples/bsor/lukas.bsor.csv", "threeCanvas_orig5");
+    players.push(player5_orig);
+    
+    const player6_orig = new MotionVisualization("samples/r_miller/original.csv", "threeCanvas_orig6");
+    players.push(player6_orig);
+        
+    for (const player of players) {
+    const controller = new PlaybackController(player, player);
+    }
 
 }
 
