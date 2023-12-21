99d905b97b59bbb4c93cdbf28e650b7756c7c46f
Marcel Roth <MarcelRoth100@aol.com>
2023-12-21 11:39:22 +0100

    update app index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,23 @@
-import {PlaybackController} from './PlaybackController'
+import {MotionVisualization} from './MotionVisualization';
+import {PlaybackController} from './PlaybackController';
 
-document.addEventListener('DOMContentLoaded', () => {
-    setupCSVLinks();
-});
 
-function setupCSVLinks(): void {
-    const csvLinks: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('#csvFileList a');
-    csvLinks.forEach(link => {
-        link.addEventListener('click', (e: MouseEvent) => {
-            e.preventDefault();
-            const target = e.target as HTMLAnchorElement;
-            const csvFilePath = target.getAttribute('data-csv');
-            if (csvFilePath) {
-                loadAnimation(csvFilePath);
-            }
-        });
-    });
+function getQueryVariable(variable: string) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        const pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
 }
 
-function loadAnimation(csvFilePath: string): void {
-    // In TypeScript, you might have to cast the `window.fetch` as any to avoid type issues
-    (window.fetch as any)('animation.html')
-        .then((response: Response) => response.text())
-        .then((html: string) => {
-            const animationContainer = document.getElementById('animation');
-            if (animationContainer) {
-                animationContainer.innerHTML = html;
-                new PlaybackController(csvFilePath);
-            }
-        })
-        .catch((error: Error) => {
-            console.error('Error loading the animation:', error);
-        });
+function initializeApp() {
+    const player1 = new MotionVisualization("samples/" + getQueryVariable("dataset") + "/original.csv", "threeCanvas1");
+    const player2 = new MotionVisualization("samples/" + getQueryVariable("dataset") + "/fixed.csv", "threeCanvas2");
+    const controller = new PlaybackController(player1, player2);
+
 }
+
+document.addEventListener('DOMContentLoaded', initializeApp);
