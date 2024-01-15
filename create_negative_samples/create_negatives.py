4d3389c8ce59cff638773740682bd4e8ca2c7485
Marcel Roth <MarcelRoth100@aol.com>
2024-01-15 17:44:20 +0100

    update; add negative beatsaber file for teaser

--- create_negative_samples/create_negatives.py
+++ create_negative_samples/create_negatives.py
@@ -71,7 +71,8 @@ def negative_units(df):
 
 if __name__ == '__main__':
     negative_vars = [
-        ("negative_coordinate_system", [negative_coordinate_system, transform_quat_to_euler, negative_coordinate_mapping]),
+        ("negative_coordinate_system", [negative_coordinate_system, transform_quat_to_euler, negative_coordinate_mapping,
+                                        transform_quat_to_euler, negative_rotations]),
         # ("negative_coordinate_mapping", [transform_quat_to_euler, negative_coordinate_mapping]),
         # ("negative_rotations", [transform_quat_to_euler, negative_rotations]),
         # ("negative_time", [negative_time]),
