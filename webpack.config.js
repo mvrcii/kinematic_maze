7974ca20b8f5925ec6a7068b0f99122c7174a944
Marcel Roth <MarcelRoth100@aol.com>
2024-10-16 15:09:07 +0200

    Update build scripts and dependencies
    Refactored `start` and `build` scripts to use specific webpack configuration files. Added `gh-deploy` script for deployment and updated dependencies including `copy-webpack-plugin`, `gh-pages`, and `webpack-merge`. Adjusted `pnpm-lock.yaml

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,42 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ],
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        // Copy all files from 'public' to 'dist'
+        { from: 'public', to: '' },
+      ],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'), // Your index.html location
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'), // This should point to where your index.html is
-        },
-        historyApiFallback: true,
-        compress: true,
-        hot: true,
-        port: 8090,
-        open: true
-    },
+    historyApiFallback: true,
+    compress: true,
+    hot: true,
+    port: 8090,
+    open: true,
+  },
 };
